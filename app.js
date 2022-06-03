5e044bf0bc494ba09769dfffae0ff3b531c6c8c6
Andrey Kiselev <mira55@yandex.ru>
2022-06-03 21:40:29 +0400

    added board

---
+++ app.js
@@ -0,0 +1,36 @@
+const board = document.querySelector('#board');
+const colors = ['#ffffff', '#f28eff', '#b81dcc', '#2cd4da', '#e635ab', '#35e6ab', '#17bdff', '#ff1770'];
+const SQUARES_NUMBER = 400;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => {
+        setColor(square);
+    });
+
+    square.addEventListener('mouseleave', () => {
+        removeColor(square);
+    });
+
+    board.append(square);
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d';
+    element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+
+    return colors[index];
+}
